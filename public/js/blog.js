8f27ad8b4e43db00d24a1f8b03517d7add39ca02
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-07-29 17:18:54 -0400

    create comment function for the blog.js

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,27 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const submitCommentBtn = document.getElementById('submit-comment-btn');
+const createNewComment = async (event) => {
+    event.preventDefault();
+    
+    const blog_id = event.target.getAttribute('data-id');
+    const description = document.querySelector('#comment').value.trim();
 
-    submitCommentBtn.addEventListener('click', async () => {
-        const commentText = document.getElementById('comment-text').value.trim();
-
-        if (commentText) {
-            try {
-                const response = await fetch('/api/comments', {
-                    method: 'POST',
-                    body: JSON.stringify({ contents: commentText }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-
-                if (!response.ok) {
-                    throw new Error('Error creating comment.');
-                }
-
-                window.location.reload();
-            } catch (err){
-                console.error(err);
-            }
+    if (description) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({description, blog_id}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(response.ok) {
+            window.location.replace(`/blog/${blog_id}`);
+        } else {
+            alert(response.statusText);
         }
-    });
-});
+    }
+};
+
+document.querySelector('#submitbtn').addEventListener('click', createNewComment);

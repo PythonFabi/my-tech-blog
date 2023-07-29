6977c07becd1185b937a025df8435db00a34e235
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-07-28 21:49:31 -0400

    add put and delete routes to the blog routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -15,5 +15,50 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
-router
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatedBlog = await Blog.update(
+            {
+                title: req.body.title, 
+                contents: req.body.contents,
+            }, 
+            {
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.user_id,
+                },
+            }
+        );
+
+        if(updatedBlog[0] === 0) {
+            res.status(403).json({ message: "You don't have permission to update this blog post" });
+            return;
+        }
+
+        res.status(200).json({ message: 'Blog post updated successfully.' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deletedBlog = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if(!deletedBlog) {
+            res.status(403).json({ message: "You don't have permission to delete this blog post." });
+            return;
+        }
+
+        res.status(200).json({ message: 'Blog post deleted successfully.' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;

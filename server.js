4fff6fad865aaee02ece874725004e282827f41e
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-08-04 00:34:12 -0400

    comment on the server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+// Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -14,12 +15,17 @@ const PORT = process.env.PORT || 3001;
 // set up the Handlebars.js engine with custom helpers --> e.g.: format_date
 const hbs = exphbs.create({ helpers });
 
+// configure and link a session object with the sequelize store
 const sess = {
     secret: 'Super secret secret',
     cookie: {
+        // maxage of cookie in milliseconds, cookie expires after 5 mins
         maxAge: 300000,
+        // cookie is accessible only through http(s) requests, not accessible by client-side javascript
         httpOnly: true,
+        // cookie is allowed to be transmitted over nonsecure http connections
         secure: false,
+        // cookie will be sent only for requests originating from the same site as the server
         sameSite: 'strict',
     },
     resave: false,
@@ -36,7 +42,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// import all files of the public folder
 app.use(express.static(path.join(__dirname, 'public')));
+// import bootstrap
 app.use('/bootstrap', express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
 
 app.use(routes);
